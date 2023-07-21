b1a9900905d7c32d6ba5726987a3b1bdcda8bda3
StoneBal <Bal0924@gmail.com>
2023-07-21 12:10:51 +0200

    1.0
    Kész weboldal

---
+++ style/jQuery.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('.arrow').hide();
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 150) {
+            $('.arrow').fadeIn();
+        } else {
+            $('.arrow').fadeOut();
+        }
+    });
+
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
