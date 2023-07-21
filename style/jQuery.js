2860e9b63d82f0e57ad16d756268d14046281e77
StoneBal <Bal0924@gmail.com>
2023-07-21 12:55:09 +0200

    footer javítás

--- style/jQuery.js
+++ style/jQuery.js
@@ -1,18 +1,18 @@
 $(document).ready(function () {
-    $('.arrow').hide();
+    $(".arrow").hide();
     $(window).scroll(function () {
         if ($(this).scrollTop() > 150) {
-            $('.arrow').fadeIn();
+            $(".arrow").fadeIn();
         } else {
-            $('.arrow').fadeOut();
+            $(".arrow").fadeOut();
         }
     });
 
-    $("a").on('click', function (event) {
+    $("a").on("click", function (event) {
         if (this.hash !== "") {
             event.preventDefault();
             var hash = this.hash;
-            $('html, body').animate({
+            $("html, body").animate({
                 scrollTop: $(hash).offset().top
             }, 700, function () {
                 window.location.hash = hash;
